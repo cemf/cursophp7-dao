e994149a8b6d859f30c47b8a5fb8fba1d1ad5571
cemf <kaikelmfabricio@gmail.com>
2019-01-28 17:21:40 -0200

    pasta e usuarios

--- index.php
+++ index.php
@@ -2,13 +2,39 @@
 
 require_once("config.php");
 
-$sql= new Sql();
 
-$usuarios = $sql ->select("SELECT * FROM tb_usuarios");
+$root = new Usuario();
 
-echo json_encode($usuarios);
+$root->loadById(3);
+		echo $root;
+
+// for($x=0;$x<=10;$x++){
+	
+	
+// 	echo $x;	
+
+// 	try {
+// 		
+// 	} catch (Exception $e) {
+// 		echo "afs $e";
+// 	}
+// }
 
 
+// echo $root."<br/>";
+
+// $teste = new Usuario();
+
+// $teste->loadById(1);
+
+// echo $teste."<br/>";
+
+
+// $sql= new Sql();
+
+// $usuarios = $sql ->select("SELECT * FROM tb_usuarios");
+
+// echo json_encode($usuarios);
 
 
 
